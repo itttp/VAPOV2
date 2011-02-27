648ffad3ccdece33a94fbba0e91c78285789d832
NorThanapon <flcl.tvboy@gmail.com>
2011-02-27 16:02:51 +0700

    This is an initial commit. The project is just started.

---
+++ Classes/VAPOV2AppDelegate.h
@@ -0,0 +1,32 @@
+//
+//  VAPOV2AppDelegate.h
+//  VAPOV2
+//
+//  Created by Thanapon Noraset on 2/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface VAPOV2AppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
